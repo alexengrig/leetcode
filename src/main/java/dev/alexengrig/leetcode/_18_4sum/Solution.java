81b3156a43b0663ad6f63bb78ab4be70420de9eb
Grig Alex <alexengrigdev@gmail.com>
2024-06-23 22:28:18 +0300

    Improve 18. 4Sum

--- src/main/java/dev/alexengrig/leetcode/_18_4sum/Solution.java
+++ src/main/java/dev/alexengrig/leetcode/_18_4sum/Solution.java
@@ -7,33 +7,51 @@ import java.util.List;
 class Solution implements Task {
     @Override
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums.length < 4) {
+            return List.of();
+        }
         Arrays.sort(nums);
-        List<List<Integer>> results = new ArrayList<>();
-        int left, right;
+        List<List<Integer>> result = new ArrayList<>();
+        int l, r;
         long sum;
         for (int i = 0; i < nums.length - 3; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             for (int j = i + 1; j < nums.length - 2; j++) {
-                if (j > i + 1 && nums[j - 1] == nums[j]) continue;
-                left = j + 1;
-                right = nums.length - 1;
-                while (left < right) {
-                    sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                l = j + 1;
+                r = nums.length - 1;
+                while (l < r) {
+                    sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
                     if (sum == target) {
-                        results.add(List.of(nums[i], nums[j], nums[left], nums[right]));
-                    }
-                    if (sum <= target) {
-                        do {
-                            left++;
-                        } while (left < right && nums[left - 1] == nums[left]);
+                        result.add(List.of(nums[i], nums[j], nums[l], nums[r]));
+                        l = skipLeft(nums, l, r);
+                        r = skipRight(nums, l, r);
+                    } else if (sum < target) {
+                        l = skipLeft(nums, l, r);
                     } else {
-                        do {
-                            right--;
-                        } while (right > left && nums[right] == nums[right + 1]);
+                        r = skipRight(nums, l, r);
                     }
                 }
             }
         }
-        return results;
+        return result;
+    }
+
+    int skipLeft(int[] nums, int l, int r) {
+        do {
+            l++;
+        } while (l < r && nums[l] == nums[l - 1]);
+        return l;
+    }
+
+    int skipRight(int[] nums, int l, int r) {
+        do {
+            r--;
+        } while (l < r && nums[r] == nums[r + 1]);
+        return r;
     }
 }
