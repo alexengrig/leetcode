cdfe4908dfb4f99defb7b5dc732d429505cfbddc
Grig Alex <alexengrigdev@gmail.com>
2025-01-14 00:10:52 +0300

    Improve map solution for 242. Valid Anagram

--- src/main/java/dev/alexengrig/leetcode/_242_valid_anagram/MapSolution.java
+++ src/main/java/dev/alexengrig/leetcode/_242_valid_anagram/MapSolution.java
@@ -9,12 +9,18 @@ class MapSolution implements Task {
         if (s.length() != t.length()) {
             return false;
         }
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-        for (int i = 0, n = s.length(); i < n; i++) {
-            sMap.compute(s.charAt(i), (k, count) -> count == null ? 1 : 1 + count);
-            tMap.compute(t.charAt(i), (k, count) -> count == null ? 1 : 1 + count);
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.compute(s.charAt(i), (key, count) -> count == null ? 1 : count + 1);
         }
-        return sMap.equals(tMap);
+        for (int i = 0; i < t.length(); i++) {
+            Integer res = map.compute(t.charAt(i), (key, count) -> count == null
+                    ? Integer.valueOf(-1)
+                    : count == 1 ? null : count - 1);
+            if (res != null && res == -1) {
+                return false;
+            }
+        }
+        return map.isEmpty();
     }
 }
