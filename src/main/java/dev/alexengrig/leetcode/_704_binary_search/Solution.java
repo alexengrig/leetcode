b58436afbee9f4844991e259060ce3a076631670
Grig Alex <alexengrigdev@gmail.com>
2025-07-08 21:42:44 +0300

    Fix java solution for 704. Binary Search

--- src/main/java/dev/alexengrig/leetcode/_704_binary_search/Solution.java
+++ src/main/java/dev/alexengrig/leetcode/_704_binary_search/Solution.java
@@ -5,7 +5,7 @@ class Solution implements Task {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1, mid;
         while (left <= right) {
-            mid = (left + right) / 2;
+            mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
