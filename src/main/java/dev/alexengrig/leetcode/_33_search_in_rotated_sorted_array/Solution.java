9d706661b8eb46c6047a2cc035cca1e09c10a37d
Grig Alex <alexengrigdev@gmail.com>
2025-07-11 20:59:44 +0300

    Improve java solution for 33. Search in Rotated Sorted Array

--- src/main/java/dev/alexengrig/leetcode/_33_search_in_rotated_sorted_array/Solution.java
+++ src/main/java/dev/alexengrig/leetcode/_33_search_in_rotated_sorted_array/Solution.java
@@ -3,19 +3,19 @@ package dev.alexengrig.leetcode._33_search_in_rotated_sorted_array;
 class Solution implements Task {
     @Override
     public int search(int[] nums, int target) {
-        int n = nums.length, left = 0, right = n - 1, mid;
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
-            mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[0] <= nums[mid]) {
-                if (nums[0] <= target && target < nums[mid]) {
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                if (nums[mid] < target && target <= nums[n - 1]) {
+                if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
