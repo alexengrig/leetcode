27d72602560a4866d75d382d8abead3b3fb6beaa
Grig Alex <alexengrigdev@gmail.com>
2025-10-28 21:46:27 +0300

    Improve python solution for 23. Merge k Sorted Lists

--- src/main/python/23_merge_k_sorted_lists/heap_solution.py
+++ src/main/python/23_merge_k_sorted_lists/heap_solution.py
@@ -7,23 +7,15 @@ from task import Task, ListNode
 class HeapSolution(Task):
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
-        for head in lists:
-            if head is not None:
-                heapq.heappush(heap, ComparableNode(head))
+        for i, head in enumerate(lists):
+            if head:
+                heapq.heappush(heap, (head.val, i, head))
         dummy = ListNode()
         node = dummy
         while len(heap):
-            min_node = heapq.heappop(heap).node
-            if min_node.next is not None:
-                heapq.heappush(heap, ComparableNode(min_node.next))
+            _, min_index, min_node = heapq.heappop(heap)
+            if min_node.next:
+                heapq.heappush(heap, (min_node.next.val, min_index + 1, min_node.next))
             node.next = min_node
             node = min_node
         return dummy.next
-
-
-class ComparableNode:
-    def __init__(self, node):
-        self.node = node
-
-    def __lt__(self, other):
-        return self.node.val < other.node.val
