d3da4f8da8a7655c2d3b8d5d17f44e861622f2ff
Grig Alex <alexengrigdev@gmail.com>
2025-10-31 22:57:48 +0300

    Add python solution for 876. Middle of the Linked List

---
+++ docs/876_middle_of_the_linked_list/README.md
@@ -0,0 +1,9 @@
+# [876. Middle of the Linked List](https://leetcode.com/problems/middle-of-the-linked-list)
+
+## Solution
+
+- [Python code](https://github.com/alexengrig/leetcode/blob/main/src/main/python/876_middle_of_the_linked_list/solution.py)
+
+**Time complexity: O(n)**
+
+**Space complexity: O(1)**
---
+++ src/main/python/876_middle_of_the_linked_list/solution.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from task import Task, ListNode
+
+
+class Solution(Task):
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
