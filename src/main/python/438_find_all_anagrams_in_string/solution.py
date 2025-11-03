c958d15306c5eea4b2957de7902ec1a9a1a26a5c
Grig Alex <alexengrigdev@gmail.com>
2025-11-03 23:43:50 +0300

    Improve python solution for 438. Find All Anagrams in a String

--- src/main/python/438_find_all_anagrams_in_string/solution.py
+++ src/main/python/438_find_all_anagrams_in_string/solution.py
@@ -6,17 +6,21 @@ from task import Task
 class Solution(Task):
     def findAnagrams(self, s: str, p: str) -> List[int]:
         res = []
-        len_s, len_p = len(s), len(p)
-        if len_s < len_p:
+        n, m = len(s), len(p)
+        if n < m:
             return res
-        cnt_s, cnt_p = Counter(s[:len_p - 1]), Counter(p)
-        for r in range(len_p - 1, len_s):
-            cnt_s[s[r]] += 1
-            l = r - len_p + 1
-            if cnt_s == cnt_p:
-                res.append(l)
-            l_ch = s[l]
-            cnt_s[l_ch] -= 1
-            if cnt_s[l_ch] == 0:
-                cnt_s.pop(l_ch)
+        counts = Counter(p)
+        l, size = 0, m
+        for r, c in enumerate(s):
+            counts[c] -= 1
+            if counts[c] >= 0:
+                size -= 1
+            while size == 0:
+                if r - l + 1 == m:
+                    res.append(l)
+                lc = s[l]
+                counts[lc] += 1
+                if counts[lc] > 0:
+                    size += 1
+                l += 1
         return res
