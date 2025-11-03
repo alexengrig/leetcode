9be5e2533a458d4648934619711210071690d91e
Grig Alex <alexengrigdev@gmail.com>
2025-11-03 17:35:02 +0300

    Improve python solution for 3. Longest Substring Without Repeating Characters

--- src/main/python/3_longest_substring_without_repeating_characters/solution.py
+++ src/main/python/3_longest_substring_without_repeating_characters/solution.py
@@ -4,11 +4,11 @@ from task import Task
 class Solution(Task):
     def lengthOfLongestSubstring(self, s: str) -> int:
         res = 0
-        count = {}
+        seen = {}
         l = 0
         for r, c in enumerate(s):
-            if c in count:
-                l = max(l, count[c] + 1)
-            count[c] = r
+            if c in seen:
+                l = max(l, seen[c] + 1)
+            seen[c] = r
             res = max(res, r - l + 1)
         return res
